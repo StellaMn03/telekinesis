25e78e9d0f472a3b60b1778b8134822889072ec8
StellaMn03 <138888432+StellaMn03@users.noreply.github.com>
2024-03-21 00:59:25 +0400

    Update main.cpp

--- Forward_list/src/main.cpp
+++ Forward_list/src/main.cpp
@@ -7,22 +7,26 @@ int main() {
     myList.push_back(1);
     myList.push_back(2);
     myList.push_back(3);
+	Forward_list<int> List2(myList);
     myList.display();
+	List2.display();
+	Forward_list<int> List3 = List2;
+	List3.display();
+
     // -1 0 1 2 3
-    myList.push_front(0);
-    myList.push_front(-1);
+  //  myList.push_front(0);
+//   myList.push_front(-1);
     //  -1 0 1
-    myList.pop_back();
-    myList.pop_back();
+ //   myList.pop_back();
+  //  myList.pop_back();
     // 0 1
-    myList.pop_front();
+ //   myList.pop_front();
     // 0 10 1
-    myList.insert(2, 10);
-    myList.remove(2);
-    myList.empty();
-    myList.clear();
-    myList.empty();
+  //  myList.insert(2, 10);
+ //   myList.remove(2);
+ //   myList.empty();
+ //   myList.clear();
+ //   myList.empty();
 
     return 0;
 }
-
