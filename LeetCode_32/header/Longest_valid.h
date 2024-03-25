4789761141925c826b4aa0a2767f4f0e662c5c96
StellaMn03 <138888432+StellaMn03@users.noreply.github.com>
2024-03-25 18:30:39 +0400

    Add files via upload

---
+++ LeetCode_32/header/Longest_valid.h
@@ -0,0 +1,13 @@
+#ifndef LONGEST_VALID_H
+#define LONGEST_VALID_H
+
+#include <string>
+
+class Longest_parentheses {
+public:
+    int longestValidParentheses(std::string s);
+};
+
+#include "Longest_valid.hpp"
+#endif // LONGEST_VALID_H
+
